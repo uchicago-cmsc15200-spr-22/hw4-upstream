8b7eb14e89e8c7fc3f30218954aff61224314731
Hannah Morgan <hannah.mairs@gmail.com>
2022-04-27 16:45:44 -0500

    Homework #4 files

--- homework4.c
+++ homework4.c
@@ -1,3 +1,25 @@
+/*
+ * Homework #4 Deliverables
+ *
+ * Name: add your name here  
+ *   YOU MUST FILL IN YOUR NAME AND THEN REMOVE THIS COMMENT.
+ *
+ * Sources used:
+ *   List all sources used to complete this assignment. Include a
+ *   URL for internet resources. You do not need to include the lab
+ *   write-up, course textbooks, or man pages. If you did not use
+ *   any sources, write the word "None".  YOU MUST COMPLETE THIS SECTION.
+ *   Please remove the explanation when you complete this field.
+ *
+ * People consulted:
+ *   List anyone you discussed this assignment with, including your
+ *   classmates, friends, and Harper Tutors.  You do not need to list
+ *   the instructors or the teaching assistants.  If you did not consult
+ *   anyone, write the work "None".  YOU MUST COMPLETE THIS SECTION.
+ *   Please remove the explanation when you complete this field.
+ *
+ */
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <assert.h>
@@ -25,6 +47,8 @@ bool valid_card(struct card card) {
  * Returns: Nothing, modifies deck in-place
  */
 void cut(struct card *deck, int num_cards) {
+    assert(deck != NULL);
+    assert(num_cards > 0);
 
     // YOUR CODE HERE
 }
@@ -39,6 +63,8 @@ void cut(struct card *deck, int num_cards) {
  *   or -1 if the hand does not contain a flush
  */
 enum suit flush(struct card *hand, int num_cards) {
+    assert(hand != NULL);
+    assert(num_cards > 0);
 
     // YOUR CODE HERE
     return -1;
@@ -54,6 +80,8 @@ enum suit flush(struct card *hand, int num_cards) {
  * Returns: true if the hand contains four-of-a-kind, false otherwise
  */
 bool four_of_a_kind(struct card *hand, int num_cards) {
+    assert(hand != NULL);
+    assert(num_cards > 0);
 
     // YOUR CODE HERE
     return false;
@@ -68,6 +96,8 @@ bool four_of_a_kind(struct card *hand, int num_cards) {
  * Returns: true if the hand busts, false otherwise
  */
 bool bust(struct card *hand, int num_cards) {
+    assert(hand != NULL);
+    assert(num_cards > 0);
 
     // YOUR CODE HERE
     return false;
@@ -86,6 +116,10 @@ bool bust(struct card *hand, int num_cards) {
  */
 int winner(struct card *hand1, int num_cards1, 
            struct card *hand2, int num_cards2) {
+    assert(hand1 != NULL);
+    assert(num_cards1 > 0);
+    assert(hand2 != NULL);
+    assert(num_cards2 > 0);
 
     // YOUR CODE HERE
     return 0;
